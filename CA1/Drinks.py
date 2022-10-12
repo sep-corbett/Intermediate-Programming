c64ff8d71b5cc17080859da27c5f49b58b043d10
Sarah Corbett <sepcorb@gmail.com>
2022-10-12 17:14:16 +0100

    Taking user input

--- CA1/Drinks.py
+++ CA1/Drinks.py
@@ -1,15 +1,16 @@
+import random 
+
 # create dictionary of drinks from text file
 drinks_dict = {}
-drinks_list = []
+menu = []
 
 with open('Cocktails.txt', 'r') as file:
     for line in file:
         line = line.strip()
         drink, ingredients = line.split(': ')
         ingredients = ingredients.split(', ')
-        # print(drink, ingredients)
         drinks_dict[drink] = ingredients
-        drinks_list.append(drink)
+        menu.append(drink)
 
 # create dictionary of ingredients by type
 ingredients_dict = {}
@@ -21,33 +22,22 @@ with open('Ingredients.txt', 'r') as file:
         ingredients = ingredients.split(', ')
         ingredients_dict[type] = ingredients
 
-# print('drinks dict')
-# for item in drinks_dict:
-#     print(item, drinks_dict[item])
-# print('\ningredients dict')
-# for item in ingredients_dict:
-#     print(item, ingredients_dict[item])
-
 class Drink:
     def __init__(self, drink_name) -> None:
         try:
-            self.name = drinks_list[int(drink_name)]
+            self.name = menu[int(drink_name)]
         except:
             self.name = drink_name.lower()
 
         self.ingredients = drinks_dict[self.name]
-        # print(self.name, self.ingredients)
         self.spirits = []
         self.mixers = []
         self.syrups = []
         self.garnishes = []
 
-        self.categorise_ingredients()
-        # if args is just number, select random drink
+        self.__categorise_ingredients()
 
-        # print((drink_name))
-
-    def categorise_ingredients(self):
+    def __categorise_ingredients(self):
         for item in self.ingredients:
             if item in ingredients_dict['spirits']:
                 self.spirits.append(item)
@@ -60,22 +50,67 @@ class Drink:
             else:
                 print(item, 'not found in ingredients file yet')
 
-    def list_ingredients(self):
-        print(self.name)
-        print('spirits: ', [item for item in self.spirits])
-        print('mixers: ', [item for item in self.mixers])
-        print('syrups: ', [item for item in self.syrups])
-        print('garnishes: ', [item for item in self.garnishes])
+    def __str__(self):
+        output = self.name
+        output += f'\nspirits: {[item for item in self.spirits]}'
+        output += f'\nmixers: {[item for item in self.mixers]}'
+        output += f'\nsyrups: {[item for item in self.syrups]}'
+        output += f'\ngarnishes: {[item for item in self.garnishes]}'
+
+        return output
+
+    def guess_ingredients(self):
+        print('Drink order:', self.name)
+        while not self.are_lists_empty():
+            self.print_hint()
+            guess = input('Guess an ingredient: ')
+            self.check_ingredient_match(guess)
+
+
+    def print_hint(self):
+        print(f'\nHint: There are {len(self.spirits)} spirits, {len(self.mixers)} mixers, {len(self.syrups)} syrups, and {len(self.garnishes)} garnishes left to guess')
+
+    def check_ingredient_match(self, guess):
+        if guess in self.spirits:
+            self.spirits.remove(guess)
+        elif guess in self.mixers:
+            self.mixers.remove(guess)
+        elif guess in self.syrups:
+            self.syrups.remove(guess)
+        elif guess in self.garnishes:
+            self.garnishes.remove(guess)
+        else:
+            print(f'{self.name} does not contain {guess}')
+
+    def are_lists_empty(self):
+        lists = [self.spirits, self.mixers, self.syrups, self.garnishes]
+        for item in lists:
+            if len(item) != 0:
+                return False
+        return True
 
 
 class Order:
-    def __init__(self) -> None:
+    def __init__(self, order_size) -> None:
         self.drinks_list = []
+        self.menu_length = len(menu)
+        self.order_size = order_size
+        print('menu length:', self.menu_length)
         
-        for i in range(5):
-            n = i # change to random number
-            # input random number in correct range
-            new_drink = Drink(drinks_list[n])
+        for i in range(self.order_size):
+            n = random.randint(0, self.menu_length - 1) 
+            new_drink = Drink(menu[n])
+            self.drinks_list.append(new_drink)
 
-mydrink = Drink(1)
-mydrink.list_ingredients()
+        
+        
+
+# mydrink = Drink(1)
+# print(mydrink)
+
+# my_order = Order(4)
+# for drink in my_order.drinks_list:
+#     print(drink, '\n')
+
+for i in range(6):
+    print(Drink(i).guess_ingredients(), '\n')
