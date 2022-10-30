e3f01a7b8ca6605a6ce83e7c456d6fc8ed704b1c
Sarah Corbett <sepcorb@gmail.com>
2022-10-30 23:00:55 +0000

    Developing game

--- CA1/Drinks.py
+++ CA1/Drinks.py
@@ -33,143 +33,141 @@ def print_all_ingredients():
 
 
 class Drink:
-    __drink_name: ClassVar[str]
-    __drink_type: ClassVar[str]
-    __ingredients: ClassVar[list]
-    # __spirits: ClassVar[list]
-    # __mixers: ClassVar[list]
-    # __syrups: ClassVar[list]
-    # __garnishes: ClassVar[list]
+    drink_name: ClassVar[str]
+    __ingredients: ClassVar[list] # hidden from the user
 
+    def __init__(self, drink_name, *args:str, **kwargs:str) -> None:
+        self.drink_name = drink_name
+        self.__ingredients = []
+        self.complete = False
+        self.current_rating = 0
 
-
-
-    def __init__(self, drink_name, drink_type, *args:str, **kwargs:str) -> None:
-        self.__drink_name = drink_name.lower()
-        
-
-        if drink_type == 'cocktail':
-            self.__ingredients = cocktails_recipes[self.drink_name]
-        elif drink_type == 'shot':
-            self.__ingredients = shots_recipes[self.drink_name]
-
-        # self.__categorise_ingredients()
-
-    # def __categorise_ingredients(self):
-    #     for item in self.__ingredients:
-    #         if item in ingredients_dict['spirits']:
-    #             self.spirits.append(item)
-    #         elif item in ingredients_dict['mixers']:
-    #             self.mixers.append(item)
-    #         elif item in ingredients_dict['syrups']:
-    #             self.syrups.append(item)
-    #         elif item in ingredients_dict['garnishes']:
-    #             self.garnishes.append(item)
-    #         else:
-    #             print(item, 'not found in ingredients file yet')
 
     def __str__(self):
-        output = self.__drink_name
-        # output += f'\nspirits: {[item for item in self.spirits]}'
-        # output += f'\nmixers: {[item for item in self.mixers]}'
-        # output += f'\nsyrups: {[item for item in self.syrups]}'
-        # output += f'\ngarnishes: {[item for item in self.garnishes]}'
-
+        output = self.drink_name
         return output
 
-    # def guess_ingredients(self):
-    #     print('Drink order:', self.name)
-    #     while not self.are_lists_empty():
-    #         self.print_hint()
-    #         guess = input('Guess an ingredient: ')
-    #         self.check_ingredient_match(guess)
+    def hint(self):
+        hint = self.__ingredients[0]
+        front_jumbled = 'b' + hint[1:]
+        end_jumbled = hint[:-1] + 'y'
+        print(f'You ask the drunken customer what they think is in a {self.drink_name}' \
+            + f'"Well I reckon there might be some uhhhh, {front_jumbled}? {end_jumbled}???"')
 
 
-    # def print_hint(self):
-    #     print(f'\nHint: There are {len(self.spirits)} spirits, {len(self.mixers)} mixers, {len(self.syrups)} syrups, and {len(self.garnishes)} garnishes left to guess')
 
-    # def check_ingredient_match(self, guess):
-    #     if guess in self.spirits:
-    #         self.spirits.remove(guess)
-    #     elif guess in self.mixers:
-    #         self.mixers.remove(guess)
-    #     elif guess in self.syrups:
-    #         self.syrups.remove(guess)
-    #     elif guess in self.garnishes:
-    #         self.garnishes.remove(guess)
-    #     else:
-    #         print(f'{self.name} does not contain {guess}')
-    #         return
-        
-    #     print("Correct!", end='')
-
-    # def are_lists_empty(self):
-    #     lists = [self.spirits, self.mixers, self.syrups, self.garnishes]
-    #     for item in lists:
-    #         if len(item) != 0:
-    #             return False
-    #     return True
-
-    def add_ingredient(self, ingredient):
+    def add(self, ingredient):
         if ingredient in self.__ingredients:
             self.__ingredients.remove(ingredient)
-            print("Good choice, {ingredient} has now been added")
+            print(f"Good choice, {ingredient} has now been added")
         else:
-            print(f"The customer tells you that there's no {ingredient} in a {self.__drink_name}. You quickly knock back the {ingredient} to hide your shame.")
+            print(f"The customer tells you that there's no {ingredient} in a {self.drink_name}. You quickly knock back the {ingredient} to hide your shame.")
 
     def add_whipped_cream(self):
         successful = bool(random.randint(0, 1))
         if successful:
             print("Odd choice, but the customer loves it! Bonus points!")
+            self.current_rating += 1
         else:
             print("The customer is lactose intolerant and afraid. Minus points.")
+            self.current_rating -= 1
+
+    def serve(self):
+        missing = len(self.__ingredients)
+        if missing > 0:
+            print(f"Are you sure you want to continue? {missing} ingredients are still missing.")
+            serving_decision = input('a: Keep preparing the drink \tb: Serve it as is \tc: Cry\n')
+            if serving_decision == 'a':
+                print("That's the spirit!")
+            elif serving_decision == 'b':
+                self.current_rating -= missing * 3
+                self.complete = True
+            elif serving_decision == 'c':
+                print("Your tears add flavour to the drink, bonus points.")
+        
+
 
 
 
 class Cocktail(Drink):
-    def __init__(self, drink_name, drink_type, *args: str, **kwargs: str) -> None:
-        super().__init__(drink_name, drink_type, *args, **kwargs)
+    def __init__(self, drink_name, *args: str, **kwargs: str) -> None:
+        super().__init__(drink_name, *args, **kwargs)
+        self.__ingredients = cocktails_recipes[self.drink_name] 
 
     def shake(self):
         print('The customer was impressed by your shaking skills, bonus points!')
+        self.current_rating += 1
 
     def add_ice(self):
         print('Ice added successfully, bonus points!')
+        self.current_rating += 1
 
 
 
 
 
 class Shot(Drink):
-    def __init__(self, drink_name, drink_type, *args: str, **kwargs: str) -> None:
-        super().__init__(drink_name, drink_type, *args, **kwargs)
+    def __init__(self, drink_name, *args: str, **kwargs: str) -> None:
+        super().__init__(drink_name, *args, **kwargs)
+        self.__ingredients = shots_recipes[self.drink_name]
 
     def shake(self):
         print("You can't shake a shot!!! Booze has gone everywhere! Minus points")
+        self.current_rating -= 1
 
     def add_ice(self):
         print("Customer has choked on your ice cube, minus points")
+        self.current_rating -= 1
 
     
 
 
-
-
-'''
 class Order:
-    def __init__(self, order_size) -> None:
+    total_score = 0
+    
+
+    def __init__(self, order_size = 5) -> None:
         self.drinks_list = []
         self.menu_length = len(menu)
         self.order_size = order_size
-        # print('menu length:', self.menu_length)
+        self.current_score = 0
 
         for i in range(self.order_size):
             n = random.randint(0, self.menu_length - 1) 
-            new_drink = Drink(menu[n])
-            self.drinks_list.append(new_drink)
+            new_drink = menu[n]
+            if new_drink in cocktails_recipes:
+                self.drinks_list.append(Cocktail(new_drink))
+            elif new_drink in shots_recipes:
+                self.drinks_list.append(Shot(new_drink))
+
+    def prepare_order(self):
+        for drink in self.drinks_list:
+
+            while not drink.complete:
+
+                try:
+                    exec(input('>>> '))
+                except:
+                    print("That input is not recognised")
+
+            score = drink.current_rating
+
+            self.total_score += score
+
+            if score < 0:
+                print('The customer is not happy with your drink, but knocks it back like a true soldier')
+            elif score == 0:
+                print('The customer is satisfied with your drink')
+            else:
+                print('The customer loves your drink and is emailing their mother about it already')
+
+
+
+            
 
 
 my_order = Order(4)
-for drink in my_order.drinks_list:
-    drink.guess_ingredients()
-'''
+# my_order.prepare_order()
+shot = Shot('scooby snack')
+shot.hint()
+
