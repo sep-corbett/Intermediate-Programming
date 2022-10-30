572d78521785961df91a65c8e1d2ef51a66b2034
Sarah Corbett <sepcorb@gmail.com>
2022-10-30 21:14:14 +0000

    Flesh out adding options

--- CA1/Drinks.py
+++ CA1/Drinks.py
@@ -36,10 +36,10 @@ class Drink:
     __drink_name: ClassVar[str]
     __drink_type: ClassVar[str]
     __ingredients: ClassVar[list]
-    __spirits: ClassVar[list]
-    __mixers: ClassVar[list]
-    __syrups: ClassVar[list]
-    __garnishes: ClassVar[list]
+    # __spirits: ClassVar[list]
+    # __mixers: ClassVar[list]
+    # __syrups: ClassVar[list]
+    # __garnishes: ClassVar[list]
 
 
 
@@ -53,62 +53,78 @@ class Drink:
         elif drink_type == 'shot':
             self.__ingredients = shots_recipes[self.drink_name]
 
-        self.__categorise_ingredients()
+        # self.__categorise_ingredients()
 
-    def __categorise_ingredients(self):
-        for item in self.__ingredients:
-            if item in ingredients_dict['spirits']:
-                self.spirits.append(item)
-            elif item in ingredients_dict['mixers']:
-                self.mixers.append(item)
-            elif item in ingredients_dict['syrups']:
-                self.syrups.append(item)
-            elif item in ingredients_dict['garnishes']:
-                self.garnishes.append(item)
-            else:
-                print(item, 'not found in ingredients file yet')
+    # def __categorise_ingredients(self):
+    #     for item in self.__ingredients:
+    #         if item in ingredients_dict['spirits']:
+    #             self.spirits.append(item)
+    #         elif item in ingredients_dict['mixers']:
+    #             self.mixers.append(item)
+    #         elif item in ingredients_dict['syrups']:
+    #             self.syrups.append(item)
+    #         elif item in ingredients_dict['garnishes']:
+    #             self.garnishes.append(item)
+    #         else:
+    #             print(item, 'not found in ingredients file yet')
 
     def __str__(self):
         output = self.__drink_name
-        output += f'\nspirits: {[item for item in self.spirits]}'
-        output += f'\nmixers: {[item for item in self.mixers]}'
-        output += f'\nsyrups: {[item for item in self.syrups]}'
-        output += f'\ngarnishes: {[item for item in self.garnishes]}'
+        # output += f'\nspirits: {[item for item in self.spirits]}'
+        # output += f'\nmixers: {[item for item in self.mixers]}'
+        # output += f'\nsyrups: {[item for item in self.syrups]}'
+        # output += f'\ngarnishes: {[item for item in self.garnishes]}'
 
         return output
 
-    def guess_ingredients(self):
-        print('Drink order:', self.name)
-        while not self.are_lists_empty():
-            self.print_hint()
-            guess = input('Guess an ingredient: ')
-            self.check_ingredient_match(guess)
+    # def guess_ingredients(self):
+    #     print('Drink order:', self.name)
+    #     while not self.are_lists_empty():
+    #         self.print_hint()
+    #         guess = input('Guess an ingredient: ')
+    #         self.check_ingredient_match(guess)
 
 
-    def print_hint(self):
-        print(f'\nHint: There are {len(self.spirits)} spirits, {len(self.mixers)} mixers, {len(self.syrups)} syrups, and {len(self.garnishes)} garnishes left to guess')
+    # def print_hint(self):
+    #     print(f'\nHint: There are {len(self.spirits)} spirits, {len(self.mixers)} mixers, {len(self.syrups)} syrups, and {len(self.garnishes)} garnishes left to guess')
 
-    def check_ingredient_match(self, guess):
-        if guess in self.spirits:
-            self.spirits.remove(guess)
-        elif guess in self.mixers:
-            self.mixers.remove(guess)
-        elif guess in self.syrups:
-            self.syrups.remove(guess)
-        elif guess in self.garnishes:
-            self.garnishes.remove(guess)
-        else:
-            print(f'{self.name} does not contain {guess}')
-            return
+    # def check_ingredient_match(self, guess):
+    #     if guess in self.spirits:
+    #         self.spirits.remove(guess)
+    #     elif guess in self.mixers:
+    #         self.mixers.remove(guess)
+    #     elif guess in self.syrups:
+    #         self.syrups.remove(guess)
+    #     elif guess in self.garnishes:
+    #         self.garnishes.remove(guess)
+    #     else:
+    #         print(f'{self.name} does not contain {guess}')
+    #         return
         
-        print("Correct!", end='')
+    #     print("Correct!", end='')
 
-    def are_lists_empty(self):
-        lists = [self.spirits, self.mixers, self.syrups, self.garnishes]
-        for item in lists:
-            if len(item) != 0:
-                return False
-        return True
+    # def are_lists_empty(self):
+    #     lists = [self.spirits, self.mixers, self.syrups, self.garnishes]
+    #     for item in lists:
+    #         if len(item) != 0:
+    #             return False
+    #     return True
+
+    def add_ingredient(self, ingredient):
+        if ingredient in self.__ingredients:
+            self.__ingredients.remove(ingredient)
+            print("Good choice, {ingredient} has now been added")
+        else:
+            print(f"The customer tells you that there's no {ingredient} in a {self.__drink_name}. You quickly knock back the {ingredient} to hide your shame.")
+
+    def add_whipped_cream(self):
+        successful = bool(random.randint(0, 1))
+        if successful:
+            print("Odd choice, but the customer loves it! Bonus points!")
+        else:
+            print("The customer is lactose intolerant and afraid. Minus points.")
+
+
 
 class Cocktail(Drink):
     def __init__(self, drink_name, drink_type, *args: str, **kwargs: str) -> None:
@@ -119,6 +135,8 @@ class Cocktail(Drink):
 
     def add_ice(self):
         print('Ice added successfully, bonus points!')
+
+
 
 
 
